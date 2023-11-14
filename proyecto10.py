d464a6ceb739137a9061e3da87e9151142c19ce7
luisvag <luisfranvag@gmail.com>
2023-11-14 12:41:19 -0400

    testing

---
+++ proyecto10.py
@@ -0,0 +1,6 @@
+import bs4
+import requests 
+
+resultado_busqueda = requests.get('https://escueladirecta-blog.blogspot.com/2023/05/configurando-la-impresion-perfecta-de.html')
+
+print(resultado_busqueda.text)
