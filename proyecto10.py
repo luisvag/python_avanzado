55b520507b35b7db09cda9f9038de1bff7510748
Luis Vasquez <137962778+luisvag@users.noreply.github.com>
2023-11-14 15:55:19 -0400

    Delete proyecto10.py

--- proyecto10.py
+++
@@ -1,7 +0,0 @@
-#librerias beautifulsoup, requests
-import bs4
-import requests 
-
-resultado_busqueda = requests.get('https://escueladirecta-blog.blogspot.com/2023/05/configurando-la-impresion-perfecta-de.html')
-
-print(resultado_busqueda.text)
